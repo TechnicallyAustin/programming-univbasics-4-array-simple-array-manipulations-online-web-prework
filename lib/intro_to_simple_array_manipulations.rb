d59d9a741f631d2b34b494e1c574a26f66733bb5
Austin Johnson <technicallyaustin@gmail.com>
2019-11-13 16:55:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -47,8 +47,9 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
 
-def using_delete_at(famous_robots, "2")
+def using_delete_at(famous_robots,deleted_robots)
   famous_robots.delete_at(2)
+  deleted_robots
   
   
   

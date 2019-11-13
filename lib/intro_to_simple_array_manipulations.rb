6b44ae1272efeec75eec78f21ea45ea2513dced7
Austin Johnson <technicallyaustin@gmail.com>
2019-11-13 16:22:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,77 @@
+def using_push(colors_in_the_rainbow,next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(boroughs_in_nyc,new_neighborhood)
+  boroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  boroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+  #deleted_string = continents.pop 
+  #deleted_string
+  #continents.size
+  #continents.pop(deleted_string)
+
+  #stores "Antarctica" as deleted String
+  
+  
+  
+  
+  
+ 
+  
+end
+
+def using_pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever","Chihuahua","Shiba Inu"]
+  small_breeds = dog_breeds.pop(2)
+  dog_breeds
+  
+  
+  
+end
+
+def using_shift 
+  
+  
+end
+
+def using_shift_with_args
+  
+  
+end
+
+def using_concat
+  
+  
+end
+
+def using_insert
+  
+  
+end
+
+def using_uniq
+  
+  
+end
+
+def using_flatten
+  
+
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+  
+end

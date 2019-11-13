50f425e57b2ac762751eefd054e166d9704eeae8
Austin Johnson <technicallyaustin@gmail.com>
2019-11-13 16:30:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def using_shift(my_favorite_cities)
   my_favorite_cities.shift
 end
 
-def using_shift_with_args(ice_cream_brands)
-  ice_cream_brands.shift(brands_removed)
+def using_shift_with_args(ice_cream_brands,2)
+  ice_cream_brands.shift
 end
 
 def using_concat(my_favorite_things)
